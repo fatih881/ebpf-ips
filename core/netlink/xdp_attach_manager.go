7186d7351986343ba100d727ea98fcfa02902063
Fatih K. <fatih39333@gmail.com>
2025-11-03 00:01:57 +0300

    Added orchestrator for single-purpose functions on attaching XDP

---
+++ core/netlink/xdp_attach_manager.go
@@ -0,0 +1,32 @@
+package netlink
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cilium/ebpf/link"
+	ebpf_export "github.com/fatih881/ebpf-ips/core/ebpf"
+)
+
+// XDPLinks is a list of all XDP link objects.
+// ActiveLinks is a map where the key is the interface index (ID) and the value is the XDP link object.
+var (
+	XDPLinks    = make([]link.Link, 0)
+	ActiveLinks = make(map[int]link.Link)
+)
+
+// Attachmanager function is the API between single purpose functions and main.go.
+// It attaches the XDP program to all the interfaces but loopback and down interfaces,this logic is implemented in findinterfaces.go
+// Returns the Active map link for potantial future use(monitoring active interfaces,cleanup etc.)
+func Attachmanager(objs *ebpf_export.IpsObjects) (map[int]link.Link, error) {
+	interfaces, err := FindInterfaces()
+	if err != nil {
+		return nil, fmt.Errorf("warning: cannot find interfaces from host : %w", err)
+	}
+	ActiveLinks, err := Attach(interfaces, objs)
+	if err != nil {
+		return nil, fmt.Errorf("warning: cannot attach interfaces to host : %w", err)
+	}
+	log.Printf("info: successfully attached XDP to %d interfaces ", len(ActiveLinks))
+	return ActiveLinks, nil
+}
