716540e7500657191f285be5f1ef575696d7ef16
Fatih K. <fatih39333@gmail.com>
2025-11-16 01:52:19 +0300

    implement(main): Added basic zap logger management

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+var logger *zap.Logger
+
 func main() {
-	/*
-		I'll start coding the main function when I have enough code to orchestrate
-	*/
+	var err error
+	logger, err = zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Can't create logger: %v", err)
+	}
+	defer func(logger *zap.Logger) {
+		err := logger.Sync()
+		if err != nil {
+			log.Fatalf("Can't stop logger: %v", err)
+		}
+	}(logger)
 }
