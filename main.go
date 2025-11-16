76c5df67c91478a2f95ae7af89cfe4a806cb2a37
Fatih K. <fatih39333@gmail.com>
2025-11-16 18:35:10 +0300

    implement(main): add channel,log and goroutine orchestration

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/signal"
 
 	"github.com/cilium/ebpf/link"
-	"github.com/fatih881/ebpf-ips/core/netlink"
+	"github.com/fatih881/ebpf-ips/core/ebpf"
+	localnl "github.com/fatih881/ebpf-ips/core/netlink"
+	"github.com/vishvananda/netlink"
 	"go.uber.org/zap"
 )
 
 var logger *zap.Logger
 
 func main() {
-	var err error
+	objs := &ebpf.IpsObjects{}
+	err := ebpf.LoadIpsObjects(objs, nil)
+	if err != nil {
+		log.Fatalf("Error loading objects: %v", err)
+		return
+	}
+	defer func(objs *ebpf.IpsObjects) {
+		err := objs.Close()
+		if err != nil {
+			log.Fatal("Error closing objects", zap.Error(err))
+			return
+		}
+	}(objs)
 	logger, err = zap.NewProduction()
 	if err != nil {
 		log.Fatalf("Can't create logger: %v", err)
@@ -22,8 +38,21 @@ func main() {
 			log.Fatalf("Can't stop logger: %v", err)
 		}
 	}(logger)
-	WriteChan := make(chan netlink.NewLink)
+	Updates := make(chan netlink.LinkUpdate)
+	done := make(chan struct{})
+	go localnl.Subscribetokernel(Updates, done, logger)
+	WriteChan := make(chan localnl.NewLink)
 	ReadChan := make(chan chan map[int]link.Link)
 	StopChan := make(chan struct{})
-	netlink.StartLinkManager(WriteChan, ReadChan, StopChan)
+	go localnl.StartLinkManager(WriteChan, ReadChan, StopChan)
+	err = localnl.AttachExistingInterfaces(objs, logger)
+	if err != nil {
+		log.Fatalf("Attach Existing Interfaces returned err : %v", err)
+		return
+	}
+	go localnl.HandleKernelMessage(Updates, ReadChan, objs, logger)
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt)
+	<-sigChan
+	close(done)
 }
